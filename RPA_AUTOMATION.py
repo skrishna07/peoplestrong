e0936df21f8430257f89d1370fc4596c45a8772f
radhirsh <sridharsukumar888@gmail.com>
2026-01-28 13:53:54 +0530

    removed unwanted imports

--- RPA_AUTOMATION.py
+++ RPA_AUTOMATION.py
@@ -1,11 +1,7 @@
-
-from modules.helpers import *
-from modules.SEND_EMAIL_SUMMARY import send_push_summary_email, send_pull_summary_email
-import curlify
-from modules.FILE_MAPPER_WITH_ERP import map_sftp_to_erp
+from libraries import *
 from modules.ERP_PULL import ERP_to_PS_Pull
 from modules.PEOPLE_STRONG_PUSH import PS_to_ERP_Push
-from libraries import *
+
 
 
 
